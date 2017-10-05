4ffd15566052b4e346ce9b09221d00e418e53610
Ellen Cooper <ellencooper74@gmail.com>
2017-10-05 10:10:06 -0600

    adds meals ajax call

--- main.bundle.js
+++ main.bundle.js
@@ -42,9 +42,20 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports) {
 
-	
+	var api_url = "https://obscure-harbor-85447.herokuapp.com/api/v1";
 
-/***/ }
+	$(document).ready(function () {
+	  $.ajax({
+	    type: "GET",
+	    url: api_url + "/meals"
+	  }).then(function (meals) {
+	    console.log(meals);
+	  }).catch(function (error) {
+	    console.log("error!!!");
+	  });
+	});
+
+/***/ })
 /******/ ]);
